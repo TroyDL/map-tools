0e99ae4866390e97297de95f872eb009bfa64602
TroyDL <troydl@gmail.com>
2013-07-31 18:52:36 -0700

    Add prototype flatten-map function

--- src/map_tools/core.clj
+++ src/map_tools/core.clj
@@ -48,57 +48,26 @@
 
 ;;;; IN PROGRESS
 
-(comment (defn ssift
-            "Given a collection of maps and a collection of key-value pairs will
-   return a collection maps where every key-value pair exists in each
-   map.
-   Example: (sift my-maps {:key1 'val1', :key2 'val2'})
-   Example: (sift user-maps {:zip 93722 :lastname 'Smith'})"
-            [coll kv]
-            (let [kv (seq kv)])
-            (loop [maps coll
-                   res ()]
-              (if maps
-                (if (= (second kv)
-                       (get-in (first maps) (first kv)))
-                  (recur (next maps)
-                         (conj res (first maps))))
-                res)))
+(defn proto-flatten-map
+  "Take map and return single level map with vector keypaths as the keys.
 
-         (defn sssift
-           ([map [k v]]
-              (sssift map (vec (list k)) v))
-           ([map keypath val]
-              (println "keypath =" keypath)
-              (if (= val (get-in map keypath))
-                map ;;if kv pair is in map, return map
-                (when (map? (get-in map keypath))
-                  (for [key (keys (get-in map keypath))]
-                    (sssift map (conj (seq keypath) key) val))))))
+   Ex:
+     (proto-flatten-map {:a {:b {:c 0} :d 1} :e 2})
+     => {[:a :b :c] 0, [:a :d] 1, [:e] 2}"
+  ([m] (proto-flatten-map m []))
+  ([m parent-key] 
+     (when m
+       (let [res (atom {})] ;; Switch to transients 
+         (doseq [[k v] m]
+           (if (map? v)
+             (if (empty? parent-key) 
+               (swap! res into (proto-flatten-map v [k]))
+               (swap! res into (proto-flatten-map v (conj parent-key k))))
+             (when (map? (select-keys m [k])) 
+               (let [[k v] (first (select-keys m [k]))
+                     k (conj parent-key k) 
+                     new-map {k v}] 
+                 (swap! res into new-map)))))
+         @res))))
 
-
-         (defn s1 [map1 key val]
-           (println map1)
-           (when (map? map1) 
-             (if (= val (get map1 key))
-               map1 ;;if kv pair is in map, return map
-               (filter map? (map (first map1) (keys (first map1)))))))
-
-         (defn pull-1st-level-maps [m1]
-           (->> m1
-                keys
-                (map m1)
-                (filter map?)))
-
-
-         (defn s0 [maps kv-map]
-           (apply (reduce s1) (seq kv-map)))
-
-         (defn sift!
-           [maps kvs]
-           (->> (map #(map (fn [kv] (sssift % kv)) (seq kvs)) maps)
-                (map first)
-                (filter not-empty)))
-
-         #_(filter not-empty (map first (map #(map (fn [kv] (sssift % kv)) kvs) maps)))
-         )
+(proto-flatten-map {:a {:b {:c 0} :d 1} :e 2})
